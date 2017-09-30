2677db1ea99249bf228c9ab43b71c6dd12202b00
Kai Curtis <kai@substantial.com>
2017-09-29 17:19:56 -0700

    Make get_yelp accessible through form
    Not the best UX at the moment, and I'd love to make this build the form
    dynamically based on meta information provided by the scraper, but that
    can wait until we have multiple scrapers, you know?

--- app.py
+++ app.py
@@ -3,12 +3,27 @@ import os
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('html'))
 
+import get_yelp
+
+
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
         data_to_show = ['Hello', 'world']
         tmpl = env.get_template('index.html')
         return tmpl.render(data=data_to_show)
+
+    @cherrypy.expose
+    def scrape(self, city, search_term, ranking, number_of_pages):
+        url_safe_search_term = search_term.replace(' ', '+')
+        scraped_data = get_yelp.get_yelp(city, search_term, ranking,
+                                         number_of_pages)
+        cherrypy.response.headers['Content-Type'] = 'text/plain'
+        cherrypy.response.headers[
+            'Content-Disposition'] = "attachment; filename='scraped_data.txt'"
+        cherrypy.response.headers['Content-Length'] = len(scraped_data)
+        return scraped_data
+
 
 config = {
     'global': {
